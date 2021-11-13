32f5e2ddf06a2a5a127d0a215ddd2c3ffd36fe76
Kosuke Izumi <ko-izumi@kddi.com>
2021-11-13 12:46:48 +0900

    Initial commit

---
+++ functions/index.js
@@ -0,0 +1,13 @@
+const functions = require('firebase-functions')
+const express = require('express')
+const basicAuth = require('basic-auth-connect')
+
+const app = express()
+
+app.all('/*', basicAuth(function(user, password) {
+  return user === 'hoge' && password === 'hogehoge';
+}));
+
+app.use(express.static(__dirname + '/web/'))
+
+exports.app = functions.https.onRequest(app)
